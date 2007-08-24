3fb844a0ff399e3fda41327449631170a0ea67d9
tri282 <tri282@f5fee5ab-3237-0410-bf1a-4129cb2bfb7e>
2007-08-24 23:54:12 +0000

    georeference parameters for webservice added:    * l,locality = locality    * hg,highergeography = highergeography    * cy, country = country    * s, stateprovince = stateprovince    * co, county = county    * vlat, verbatimlatitude = verbatimlatitude    * vlng, verbatimlongitude = verbatimlongitude    * is, island = island    * ig, islandgroup = islandgroup    * w, waterbody = waterbody    * c, continent = continent    * i, interpreter = interpreter (this does not get added to the record)    *

---
+++ bgws/src/edu/berkeley/biogeomancer/webservice/server/services/SingleGeorefServlet.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2007 University of California at Berkeley.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package edu.berkeley.biogeomancer.webservice.server.services;
+
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.biogeomancer.records.Rec;
+
+import edu.berkeley.biogeomancer.webservice.server.util.BgUtil;
+
+/**
+ * Web service for georeferencing a single locality.
+ * 
+ */
+public class SingleGeorefServlet extends HttpServlet {
+
+  Logger log = Logger.getLogger(SingleGeorefServlet.class);
+
+  private final BgUtil bgUtil = new BgUtil();
+
+  /**
+   * Georeferences a single locality using the BioGeomancer Core API. Returns
+   * all generated georeferences as XML.
+   */
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, java.io.IOException {
+    response.setContentType("text/xml");
+    //String locality = request.getParameter("l");
+    //String higherGeography = request.getParameter("hg");
+    String interpreter = request.getParameter("i");
+    if (interpreter == null) interpreter = request.getParameter("interpreter");
+    if (interpreter == null || interpreter.equals(""))
+    	interpreter = "yale";
+    
+    Rec recordMap = getParameters(request);
+
+    /*
+     * log.info("Locality: " + locality + " HigherGeography: " + higherGeography + "
+     * Interpreter: " + interpreter);
+     */
+
+    PrintWriter out = response.getWriter();
+
+    // Build the XML header.
+    out.println("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+    out.println("<biogeomancer xmlns:dwc=\"http://rs.tdwg.org/tapir/1.0\">");
+
+    out.println("<interpreter>" + interpreter + "</interpreter>");
+    //BgUtil.buildSingleXmlText(locality, higherGeography, interpreter, out);
+    BgUtil.buildSingleXmlText(recordMap, interpreter, out);
+    out.println("</biogeomancer>");
+  }
+  /**
+   * 
+   * @param request
+   * @return Rec base on given request
+   * helper function for doGet
+   * get support url parameters:
+   * l,locality = locality
+   * hg,highergeography = highergeography
+   * cy, country = country
+   * s, stateprovince = stateprovince
+   * co, county = county
+   * vlat, verbatimlatitude = verbatimlatitude
+   * vlng, verbatimlongitude = verbatimlongitude
+   * is, island = island
+   * ig, islandgroup = islandgroup
+   * w, waterbody = waterbody
+   * c, continent = continent
+   * i, interpreter = interpreter (this does not get added to the record)
+   */  
+  private Rec getParameters(HttpServletRequest request)
+  {
+	  Rec rec = new Rec();
+
+	  String requestParam = request.getParameter("l");
+	  if (requestParam == null) requestParam = request.getParameter("locality");
+	  put(rec, "locality", requestParam);
+	  
+	  requestParam = request.getParameter("hg");
+	  if (requestParam == null) requestParam = request.getParameter("highergeography");
+	  put(rec, "highergeography", requestParam);
+	  
+	  requestParam = request.getParameter("cy");
+	  if (requestParam == null) requestParam = request.getParameter("country");
+	  put(rec, "country", requestParam);
+	  
+	  requestParam = request.getParameter("s");
+	  if (requestParam == null) requestParam = request.getParameter("stateprovince");
+	  put(rec, "stateprovince", requestParam);
+	  
+	  requestParam = request.getParameter("co");
+	  if (requestParam == null) requestParam = request.getParameter("county");
+	  put(rec, "county", requestParam);
+	  
+	  requestParam = request.getParameter("vlat");
+	  if (requestParam == null) requestParam = request.getParameter("verbatimlatitude");
+	  put(rec, "verbatimlatitude", requestParam);
+	  
+	  requestParam = request.getParameter("vlng");
+	  if (requestParam == null) requestParam = request.getParameter("verbatimlongitude");
+	  put(rec, "verbatimlongitude", requestParam);
+	  
+	  requestParam = request.getParameter("is");
+	  if (requestParam == null) requestParam = request.getParameter("island");
+	  put(rec, "island", requestParam);
+	  
+	  requestParam = request.getParameter("ig");
+	  if (requestParam == null) requestParam = request.getParameter("islandgroup");
+	  put(rec, "islandgroup", requestParam);
+	  
+	  requestParam = request.getParameter("w");
+	  if (requestParam == null) requestParam = request.getParameter("waterbody");
+	  put(rec, "waterbody", requestParam);
+	  
+	  requestParam = request.getParameter("cn");
+	  if (requestParam == null) requestParam = request.getParameter("continent");
+	  put(rec, "continent", requestParam);
+	  return rec;
+  }
+  /**
+   * @param recMapping
+   * @param key
+   * @param value
+   * helper function for getParamerters: 
+   * put the value along with its key Record if value != null
+   */
+  private void put(Rec recMapping, String key, String value)
+  {
+	  if (value != null)
+		  recMapping.put(key, value);
+  }
+}
---
+++ bgws/src/edu/berkeley/biogeomancer/webservice/server/util/BgUtil.java
@@ -0,0 +1,220 @@
+package edu.berkeley.biogeomancer.webservice.server.util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.biogeomancer.managers.GeorefManager;
+import org.biogeomancer.managers.GeorefPreferences;
+import org.biogeomancer.managers.GeorefManager.GeorefManagerException;
+import org.biogeomancer.records.Georef;
+import org.biogeomancer.records.Rec;
+import org.biogeomancer.records.RecSet;
+import org.biogeomancer.records.RecSet.RecSetException;
+
+/**
+ * Utility class for wrapping BioGeomancer Core API.
+ */
+public class BgUtil {
+
+  private static Logger log = Logger.getLogger(BgUtil.class);
+
+  /**
+   * 
+   * @param String locality
+   * @param String higherGeography
+   * @param String interpreter
+   * @param String out create a list of Georefs from locality, higherGeography,
+   *          and interpreter output xml tag and value for these georeferences
+   *          in format: <dwc:Locality>LocalityName</dwc:Locality>
+   *          <dwc:HigherGeography>HigherGeographyName</dwc:HigherGeography>
+   *          <georeference> <dwc:DecimalLatitude>value</dwc:DecimalLatitude>
+   *          <dwc:DecimalLongitude>value</dwc:DecimalLongitude>
+   *          <dwc:CoordinateUncertaintyInMeters>value</dwc:CoordinateUncertaintyInMeters>
+   *          </georeference> more georeferences here if there are more
+   */
+  public static void buildSingleXmlText(String locality,
+      String higherGeography, String interpreter, PrintWriter out) {
+    log.info("Locality: " + locality + " HigherGeography: " + higherGeography
+        + " Interpreter: " + interpreter);
+    out.println("<dwc:Locality>" + locality + "</dwc:Locality>");
+    out.println("<dwc:HigherGeography>" + higherGeography
+        + "</dwc:HigherGeography>");
+    List<Georef> georefs = georeference(locality, higherGeography, interpreter);
+    for (Georef g : georefs) {
+      out.println("<georeference>");
+      out.println("<dwc:DecimalLatitude>" + g.pointRadius.y
+          + "</dwc:DecimalLatitude>");
+      out.println("<dwc:DecimalLongitude>" + g.pointRadius.x
+          + "</dwc:DecimalLongitude>");
+      out.println("<dwc:CoordinateUncertaintyInMeters>" + g.pointRadius.extent
+          + "</dwc:CoordinateUncertaintyInMeters>");
+      out.println("</georeference>");
+    }
+  }
+  /**
+   * @param Rec rec
+   * @param String interpreter (for georeference)
+   * @param PrintWiter out: to output xml text to the browser
+   * build xml text base on record rec
+   *
+   */
+  public static void buildSingleXmlText(Rec rec, String interpreter, PrintWriter out)
+  {
+	  Iterator<String> georefFields = rec.keySet().iterator();
+	  //building dwc header for each georeference
+	  while(georefFields.hasNext())
+	  {
+		  String georefField = georefFields.next();
+		  String georefValue = rec.get(georefField);
+		  log.info(georefField + ": " + georefValue + " ");
+		  String dwcFieldTag = "dwc:" + georefField + ">";
+		  out.println("<" + dwcFieldTag + georefValue + "</" + dwcFieldTag);
+	  }
+	  List<Georef> georefs = georeference(rec, interpreter);
+	  //display DecimalLatitude DecimalLongitude CoordinateUncertaintyInMeters
+	  // values in xml format
+	  for (Georef g : georefs) {
+	      out.println("<georeference>");
+	      out.println("<dwc:DecimalLatitude>" + g.pointRadius.y
+	          + "</dwc:DecimalLatitude>");
+	      out.println("<dwc:DecimalLongitude>" + g.pointRadius.x
+	          + "</dwc:DecimalLongitude>");
+	      out.println("<dwc:CoordinateUncertaintyInMeters>" + g.pointRadius.extent
+	          + "</dwc:CoordinateUncertaintyInMeters>");
+	      out.println("</georeference>");
+	    }
+  }
+  /**
+   * 
+   * @param String locality
+   * @param String higherGeography
+   * @param String interpreter
+   * @param StringBuilder response
+   * generate single georeference and append it to response
+   */
+  public static void buildSingleGeoreference(String locality,
+	      String higherGeography, String interpreter, StringBuilder response) {
+	    log.info("Locality: " + locality + " HigherGeography: " + higherGeography
+	        + " Interpreter: " + interpreter);
+	    List<Georef> georefs = georeference(locality, higherGeography, interpreter);
+	    response.append("Locality: " + locality + " HigherGeography: " + higherGeography
+	            + " Interpreter: " + interpreter + "<BR>");
+	    for (Georef g : georefs) {
+		    response.append("Georeference: ");
+		    response.append("DecimalLatitude=" + g.pointRadius.y + "<BR>");
+		    response.append("DecimalLongitude=" + g.pointRadius.x + "<BR>");
+		    response.append("CoordinateUncertaintyInMeters=" + g.pointRadius.extent+ "<P>");
+	  }
+  }
+
+  /**
+   * Georeferences the list of recs using BioGeomancer Core API.
+   * 
+   * @param recs the list of Rec objects to georeference
+   * @return the georeferences
+   */
+  public static List<Georef> georeference(String FileName, String interpreter) {
+    // take FileName argument and interperter name
+    // get RecSet from the file and return Georef List
+    // has not tested yet
+    try {
+      RecSet referenceSet = new RecSet(FileName, "\t");
+      // Iterator<Rec> recIter = referenceSet.recs.iterator();
+      List<Georef> recsList = new ArrayList<Georef>();
+      for (Iterator<Rec> recIter = referenceSet.recs.iterator(); recIter
+          .hasNext();) {
+        Rec currentRec = recIter.next();
+        GeorefManager gm;
+        try {
+          gm = new GeorefManager();
+          gm.georeference(currentRec, new GeorefPreferences(interpreter));
+          recsList.addAll(currentRec.georefs);
+        } catch (GeorefManagerException e) {
+          // TODO: Logging error to an error log
+          e.printStackTrace();
+        }
+
+      }
+      return recsList;
+    } catch (RecSetException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return null;
+    }
+
+  }
+
+  /**
+   * Returns a list of Georef objects generated using BioGeomancer Core API. If
+   * there is an error or if no georeferences were generated, returns null.
+   * 
+   * @param locality the locality to georeference
+   * @param higherGeography the higher geography to georeference
+   * @param interpreter the BioGeomancer locality intepreter to use
+   * @return List<Georef> the generated georeferences
+   */
+  public static List<Georef> georeference(String locality,
+      String higherGeography, String interpreter) {
+
+    // Default interpreter is Yale.
+    if (interpreter == null || interpreter.equals("")) {
+      interpreter = "yale";
+    }
+
+    final Rec rec = new Rec();
+    rec.put("locality", locality);
+    rec.put("highergeography", higherGeography);
+    GeorefManager gm;
+    try {
+      gm = new GeorefManager();
+      gm.georeference(rec, new GeorefPreferences(interpreter));
+      return rec.georefs;
+    } catch (GeorefManagerException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+  /**
+   * 
+   * @param rec
+   * @param interpreter
+   * @return list of Georef from given record and interpreter
+   */
+  public static List<Georef> georeference(Rec rec, String interpreter)
+  {
+	  GeorefManager gm;
+	  try {
+	      gm = new GeorefManager();
+	      gm.georeference(rec, new GeorefPreferences(interpreter));
+	      return rec.georefs;
+	  } catch (GeorefManagerException e) {
+	      e.printStackTrace();
+	      return null;
+	  }	  
+	  
+  }
+
+  /**
+   * 
+   * @param fileName
+   * @param data write string of data to a file
+   */
+  public static void recordToFile(String fileName, String data) {
+    try {
+      BufferedWriter buff = new BufferedWriter(new FileWriter(fileName));
+      buff.write(data);
+      buff.close();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
+}
